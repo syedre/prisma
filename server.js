4a95bc9bd63f9980db2b9848f0527f5c02ad9f82
syedre <syed.rehan123@gmail.com>
2025-10-31 17:43:04 +0530

    connected to local db

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-import "dotenv/config";
-
+import { PrismaClient } from "@prisma/client";
 import express from "express";
+
 const app = express();
-const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   return res.send("Hi  guys ");
 });
 
-app.listen(PORT, () => console.log(`server os running on  ${PORT}`));
+app.get("/users", async (req, res) => {
+  const users = await prisma.users.findMany();
+  return res.json(users);
+});
+
+const prisma = new PrismaClient();
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`server os running on a  ${PORT}`));
